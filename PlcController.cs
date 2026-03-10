06a1359601940240ff88788e33194e6f8ded5076
QuocHuy <kuuhaku2810@gmail.com>
2026-03-10 12:48:10 +0700

    testgit

---
+++ PlcController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using S7.Net;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlcController : ControllerBase
+{
+    private static Plc _plc = new Plc(CpuType.S71200, "192.168.1.10", 0, 1);
+
+    [HttpGet("data")]
+    public IActionResult GetPlcData()
+    {
+        try
+        {
+            if (!_plc.IsConnected) _plc.Open();
+
+            // Đọc dữ liệu từ DB1 (Ví dụ)
+            var data = new PlcDataDto
+            {
+                MotorStatus = (bool)_plc.Read("DB1.DBX0.0"),
+                Speed = (short)_plc.Read("DB1.DBW2"),
+                Temperature = ((uint)_plc.Read("DB1.DBD4")).ToFloat(),
+                ProductionCount = (int)_plc.Read("DB1.DBD8"),
+                MachineName = "SIEMENS_LINE_01"
+            };
+
+            return Ok(data);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
